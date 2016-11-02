c21ffa273cbda88c4ecf1c4d74640333efac61c2
Dmitry Bykovsiy <apaximperator@gmail.com>
2016-11-02 18:14:29 +0200

    Create Dmyto_Bykvskiy_test.js

---
+++ lesson3/Dmyto_Bykvskiy_test.js
@@ -0,0 +1,150 @@
+/* Сравнение объектов по свойству
+1. Напишите функцию compareObjects, которая принимает 2 объекта и название числового свойства, 
+по которому нужно выполнить сравнение объектов, и выводит в консоль свойство name того объекта,  
+у которого значение переданного свойства больше.
+2. Создайте один объект с помощью литерала, второй через конструктор.
+3. Вызовите написанную функцию и передайте два созданных объекта и свойство для сравнения
+*/
+
+var o1 = {
+    myProperty: 456,
+    name: 'First Object'
+};
+var o2 = new ObjectMachine();
+var result = compareObjects(o1, o2, 'myProperty');
+
+console.log(result);
+
+function ObjectMachine() {
+    this.myProperty = 789;
+    this.name = 'Second Object'
+}
+
+function compareObjects(obj1, obj2, numProp) {
+    return obj1[numProp] > obj2[numProp] ? obj1.name : obj2.name;
+}
+
+
+/*
+Поиск любимой песни
+1. Создайте коллекцию из 5 музыкальных песен, где каждая песня содержит следующую информацию: 
+played - количество раз песня была проиграна (определить случайным образом), name - имя песни
+2. Напишите функцию favoriteSong, которая принимает коллекцию из песен, и возвращает следующую информацию: 
+название песни, сколько раз песня была проиграна, индекс песни в коллекции.
+3. Вызовите функцию favoriteSong и передайте ей созданную коллекцию
+*/
+
+var mySongNames = [
+    'Apollo Brown - Choices', 
+    'White Stripes – Seven Nation Army',
+    'Chase & Status – Big Man', 
+    'Etienne De Crecy – FUNK (The Bloody Beetroots Remix)', 
+    'Raphael Saadiq - Good Man'];
+
+var myCollection = createSongCollection(mySongNames);
+favoriteSong(myCollection);
+
+function Song(name) {
+    this.played = Math.floor(Math.random() * 300);
+    this.name = name;
+}
+
+function createSongCollection(songNames) {
+    var collection = [];
+
+    for (var i = 0; i < 5; i++) {
+        collection.push(new Song(songNames[i]));
+    }
+
+    return collection;
+}
+
+function favoriteSong(collection) {
+    var favSong = collection[0];
+    var favSongIndex = 0;
+    
+    for (var i = 1; i < collection.length; i++) {
+        if (collection[i].played > favSong.played) {
+            favSong = collection[i];
+            favSongIndex = i;
+        }
+    }
+
+    var favSongInfo = {
+        name: favSong.name,
+        played: favSong.played,
+        index: favSongIndex
+    }
+
+    return favSongInfo;
+}
+
+
+/*
+ Класс калькулятор
+ 1. Опишите конструктор объектов (класс) Calculator с двумя методами:
+ - add - принимает число и прибавляет его к предыдущему,
+ - getCurrentSum - принимает индекс и возвращает результирующее число на шаге указынном в индексе
+ (если индекса нет, возвращает текущую сумму)
+ 2. Создайте два экземпляра класса Calculator
+ 3. Добавьте в первый объект числа 3,8,11 и во второй 5,12,17.
+ 4. Выведите в консоль сумму:
+ - всех чисел всех объектов, убедитесь (56)
+ - сумму чисел всех объектов на втором шаге (28)
+ - для одного объекта сумму после третьего шага и общую результирующую сумму (должна совпадать)
+
+ @constructor
+ @returns {Number}
+ 
+ Пример вызова:
+ var calc1 = new Calculator(3, 8, 11);
+ expect(calc1.getCurrentSum()).toBe(22);
+ expect(calc2.add(8)).toBe(30);
+ */
+
+function Calculator() {
+    var Arguments = [];
+
+    for (var i = 0; i < arguments.length; i++) {
+        Arguments.push(arguments[i]);
+    }
+
+    var sum = [];
+
+    this.add = function (num) {
+        if (typeof num === 'number' && !isNaN(num)) {
+            Arguments.push(num);
+            sum = Arguments.reduce(sumAccumulator);
+
+            return this;
+        } else {
+            throw new Error('Please, enter a number.');
+        }
+    };
+
+    this.getCurrentSum = function (index) {
+        if (!Arguments.length) {
+            return 0;
+        }
+        if (index === 'undefined') {
+            return Arguments.reduce(sumAccumulator);
+        }
+
+        return Arguments.slice(0, index).reduce(sumAccumulator);
+    };
+
+    function sumAccumulator(a, b) {
+        return a + b;
+    }
+}
+
+var calc1 = new Calculator(3, 8, 11);
+var calc2 = new Calculator(5, 12, 17);
+
+console.log('calc1 + calc2 = ' + (calc1.getCurrentSum() + calc2.getCurrentSum()));
+console.log('Including to second index, calc1 + calc2 = ' + (calc1.getCurrentSum(2) + calc2.getCurrentSum(2)));
+console.log('calc1.getCurrentSum(3) === calc1.getCurrentSum() is ' + (calc1.getCurrentSum(3) === calc1.getCurrentSum()));
+console.log('calc1 = ' + calc1.getCurrentSum());
+
+calc1.add(3).add(8).add(11);
+console.log('calc1 + 3 + 8 + 11 = ' + calc1.getCurrentSum());
